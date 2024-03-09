0601d7b428d8054eade80372852a92ebeeb815c1
Lorenz Kuehl <lorenz.kuehl.coding@gmail.com>
2024-03-09 17:51:53 +0100

    initial commit

---
+++ src/views/NewRideView.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <!-- Überschrift -->
+    <h1>Deine nächste Fahrt</h1>
+  </div>
+  <div>
+    <!-- Text-Input für die Event-Description, später PUT zur API -->
+    <input
+      type="text"
+      name="eventDescription"
+      id="event-description"
+      placeholder="Name der Fahrt"
+    />
+  </div>
+  <div>
+    <!-- Datums-Input für das Event, Start und Ende, später PUT zur API, 
+        Format anpassen zur Darstellung in der Calendar-Component -->
+    <input type="datetime" name="eventStart" id="event-start" />
+    <input type="datetime" name="eventEnd" id="event-end" />
+  </div>
+  <div>
+    <!-- MitfahrerInnen-Input für das Event, am besten dynamisch erzeugt 
+        aus der Auto-Gruppe anhand der driver_id, mit Darstellung des Buchstaben-
+        Icons, später PUT zur API, if checked -> eventParticipants -->
+    <select name="eventParticipants" id="event-participants">
+      <option value="MitfahrerInnen hinzufügen"></option>
+      <option value="driver_id = 1">
+        <input type="checkbox" name="eventParticipants" id="driver-1" />
+      </option>
+      <option value="driver_id = 2">
+        <input type="checkbox" name="eventParticipants" id="driver-2" />
+      </option>
+      <option value="driver_id = 3">
+        <input type="checkbox" name="eventParticipants" id="driver-3" />
+      </option>
+      <option value="driver_id = 4">
+        <input type="checkbox" name="eventParticipants" id="driver-4" />
+      </option>
+    </select>
+  </div>
+  <div>
+    <!--Auswahl des EventType, später PUT zur API, eventType -->
+    <input type="radio" name="eventType" id="freie-fahrt" /><span>oder</span
+    ><input type="radio" name="eventType" id="muss-sein" />
+  </div>
+  <div>
+    <!-- Evt. Button zum Abbrechen einfügen? + Submit -Button um die Fahrt zu erstellen-->
+    <button>ABBRECHEN</button><button type="submit">FAHRT ERSTELLEN</button>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped></style>
