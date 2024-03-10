9ce8d4af5f20b0c4f9474f07b86f3e17b46889d3
dorojuse <doroseitz@yahoo.com>
2024-03-10 20:36:06 +0100

    first steps home

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,34 @@
 <template>
-  <div></div>
+  <main>
+    <div>Platzhalter Menü</div>
+    <div>Platzhalter Icon Name</div>
+    <h2>Hi Maria!</h2>
+    <!--das muss dann mit {{ username }} dynamisiert werden-->
+    <p>Dein Fahrt startet:</p>
+    <p>Heute!</p>
+    <!--bzw. hier das Datum der jeweils nächsten Fahrt des Users-->
+    <div>Platzhalter Kalender</div>
+    <p>Dein Auto: Berta</p>
+    <p>Kilometerstand</p>
+    <p>Deine carma-Points</p>
+    <p>Danke, dass du dabei bist!</p>
+    <p>carma</p>
+  </main>
 </template>
 
 <script>
 export default {}
 </script>
+<!--Nach dem Login sollten wir wissen, welcher User angemeldet ist 
+  und so auf die weiteren Daten zugreifen. Wie das genau funktioniert, 
+  weiß ich gerade leider nicht.  
+-->
 
-<style lang="scss" scoped></style>
+<style scoped></style>
+
+<!--Next Steps:
+Menü und Kalender Components einbauen
+Styling
+Username und Datum nächste Fahrt aus Datenbank fetchen
+Autoname, Kilometerstand und carma-Punktestand aus Datenbank fetchen
+-->
