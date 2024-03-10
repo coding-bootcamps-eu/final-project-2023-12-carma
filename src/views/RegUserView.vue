e69ab8ebff5ee1621b19db0f45e23b73b5a850a6
Christoph Menningen <christoph.menningen@gmail.com>
2024-03-10 21:24:41 +0100

    angelegt, bisher nur HTML

---
+++ src/views/RegUserView.vue
@@ -0,0 +1,25 @@
+<template>
+    <main>
+        <button><RouterLink to="/">x</RouterLink></button>
+        <h2>Stell dich vor!</h2>
+       <form>
+            <input type="email" name="email" placeholder="E-Mail Adresse" />
+            <input type="password" name="password" placeholder="Passwort" minlength="8" required /> //Länge und Vorgaben des Passworts
+            <input type="text" name="firstname" placeholder="Vorname" />
+            <input type="text" name="lastname" placeholder="Nachname" />
+       </form>
+       <button><router-link to="">Auto-Gruppe erstellen</router-link>≈</button>
+       <button><router-link to="">Auto-Gruppe beitreten</router-link>≈</button>
+       <p>Carma</p>
+    </main>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
