ef1db442350e133437e162bfebb37257b58cbc4b
dorojuse <doroseitz@yahoo.com>
2024-03-10 20:02:27 +0100

    first steps login

---
+++ src/views/LoginView.vue
@@ -0,0 +1,28 @@
+<template>
+  <main>
+    <button><RouterLink to="/">x</RouterLink></button>
+    <h2>Willkommen zurück!</h2>
+    <form>
+      <input type="text" name="username" placeholder="Benutzer*innen-Name" />
+      <!--Frage: Brauchen wir ein Label oder "reicht" ein placeholder?-->
+      <input type="text" name="password" placeholder="Passwort" />
+      <!--Frage: Brauchen wir ein Label oder "reicht" ein placeholder?-->
+      <input type="submit" /><RouterLink to="/home">Log in</RouterLink>/>
+    </form>
+    <p>carma</p>
+  </main>
+</template>
+
+<script>
+//Daten müssen auf Richtigkeit geprüft werden, sonst Fehlermeldung
+export default {}
+</script>
+
+<style scoped></style>
+
+<!--Next Steps:
+Einbinden Hintergrund Bild
+Custom Styles (Schrift, Farben, Buttons)
+Ausprobieren, ob die Links richtig funktionieren
+Wie kommen die Daten in die Datenbank? (siehe "action" und "method" Attribut von html input tags)
+-->
