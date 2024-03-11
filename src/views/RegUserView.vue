bf7efd92330cea84fd5e8a12b971504385a8127e
Christoph Menningen <christoph.menningen@gmail.com>
2024-03-11 12:12:00 +0100

    Korrekturen laut Eurer Kommentare

--- src/views/RegUserView.vue
+++ src/views/RegUserView.vue
@@ -3,14 +3,14 @@
         <button><RouterLink to="/">x</RouterLink></button>
         <h1>Stell dich vor!</h1>
        <form>
-            <input type="email" name="email" placeholder="E-Mail Adresse" />
+            <input type="email" name="email" placeholder="E-Mail Adresse" required/>
             <input type="password" name="password" placeholder="Passwort" minlength="8" required /> //Länge und Vorgaben des Passworts
-            <input type="passwordConfirm" name="text" placeholder="Passwort bestätigen"/> 
-            <input type="text" name="firstname" placeholder="Vorname" maxlength="14" style="text-transform: capitalize;"/>
-            <input type="text" name="lastname" placeholder="Nachname" style="text-transform: capitalize;"/>
+            <input type="password" name="passwordConfirm" placeholder="Passwort bestätigen" required/> 
+            <input type="text" name="firstname" placeholder="Vorname" maxlength="14" style="text-transform: capitalize;" required/>
+            <input type="text" name="lastname" placeholder="Nachname" style="text-transform: capitalize;"required/>
        </form>
-       <button><router-link to="/register/car">Auto-Gruppe erstellen</router-link>≈</button>
-       <button><router-link to="/join">Auto-Gruppe beitreten</router-link>≈</button>
+       <button><router-link to="/register/car">Auto-Gruppe erstellen</router-link></button>
+       <button><router-link to="/join">Auto-Gruppe beitreten</router-link></button>
        <p>Carma</p>
     </main>
 </template>
