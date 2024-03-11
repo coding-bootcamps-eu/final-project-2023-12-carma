1e9d6a50008b58fa93f6b1e4af9c55294929ec8c
dorojuse <doroseitz@yahoo.com>
2024-03-11 11:41:08 +0100

    initial commit

---
+++ src/views/CalendarView.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <button>zurück</button>
+    <div>Menü</div>
+    <div>Icon User</div>
+    <div>Kalender wird hier eingebunden</div>
+    <button>+</button>
+    <ul>
+      <li></li>
+    </ul>
+  </main>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style scoped></style>
