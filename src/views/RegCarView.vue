3bc7da1cf6cff0a2f12947ade738793d8d66e64e
Christoph Menningen <christoph.menningen@gmail.com>
2024-03-11 14:33:57 +0100

    erster Commit, HTML erstellt

---
+++ src/views/RegCarView.vue
@@ -0,0 +1,24 @@
+<template>
+    <main>
+        <button><RouterLink to="/register">back</RouterLink></button>
+        <h1>Stell dein Auto vor!</h1>
+       <form>
+            <input type="text" name="autoname" placeholder="Autoname" required/>
+            <input type="text" name="autokennzeichen" placeholder="Autokennzeichen" maxlength="11" required />
+            <input type="number" name="tachostand" placeholder="Aktueller Tachostand" required/> 
+            <input type="date" name="waschtermin" placeholder="Letzter Waschtermin" />
+       </form>
+       <button>los geht's!</button>
+       <p>Carma</p>
+    </main>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+</style>
