552414a9f38a6a44a64f76346fd81480db127eaf
dorojuse <doroseitz@yahoo.com>
2024-03-11 13:12:09 +0100

    calendar-view

--- src/views/CalendarView.vue
+++ src/views/CalendarView.vue
@@ -4,9 +4,20 @@
     <div>Menü</div>
     <div>Icon User</div>
     <div>Kalender wird hier eingebunden</div>
-    <button>+</button>
+    <button><RouterLink to="/new-ride">+</RouterLink></button>
     <ul>
-      <li></li>
+      <li>
+        <div>Icon User</div>
+        <p>
+          {{ eventDescription }}
+        </p>
+        <p>{{ eventStart + '-' + eventEnd }}</p>
+        <button><RouterLink to="/edit-ride">edit</RouterLink></button>
+        <button>delete</button>
+        <!--hier muss das jeweilige li aus der ul entfernt werden,
+        vorher noch einen alert "wirklich löschen?", das könnte man in einer method anlegen, die @click dann ausgeführt wird-->
+      </li>
+      <!--hier müssen dynamisch Events geadded und neue "li" erstellt werden können-->
     </ul>
   </main>
 </template>
@@ -16,3 +27,10 @@ export default {}
 </script>
 
 <style scoped></style>
+
+<!--Next Steps:
+Menü, Kalender, Header, Footer einbinden
+Daten fetchen
+Methods anlegen (so ähnlich wie addTodo bei der Liste, wenn das neue Event "submitted" wird, muss es in der Liste erscheinen)
+Delete-Overlay
+-->
