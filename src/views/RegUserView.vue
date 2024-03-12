cb9a61494086dcdb4f57ee349aa460f1b1a41159
Christoph Menningen <christoph.menningen@gmail.com>
2024-03-12 15:16:12 +0100

    DeleteRide gelöscht, Home, Login, RegCar und RegUser editiert. main, menü-platzhalter, carma gelöscht, alles auf aktuellen Figma Stand gebracht

--- src/views/RegUserView.vue
+++ src/views/RegUserView.vue
@@ -1,32 +1,31 @@
+<!-- cm-->
+
 <template>
-  <main>
-    <button><RouterLink to="/">x</RouterLink></button>
-    <h1>Stell dich vor!</h1>
-    <form>
-      <input type="email" name="email" placeholder="E-Mail Adresse" required />
-      <input type="password" name="password" placeholder="Passwort" minlength="8" required />
-      //Länge und Vorgaben des Passworts
-      <input type="password" name="passwordConfirm" placeholder="Passwort bestätigen" required />
-      <input
-        type="text"
-        name="firstname"
-        placeholder="Vorname"
-        maxlength="14"
-        style="text-transform: capitalize"
-        required
-      />
-      <input
-        type="text"
-        name="lastname"
-        placeholder="Nachname"
-        style="text-transform: capitalize"
-        required
-      />
-    </form>
-    <button><router-link to="/register/car">Auto-Gruppe erstellen</router-link></button>
-    <button><router-link to="/join">Auto-Gruppe beitreten</router-link></button>
-    <p>Carma</p>
-  </main>
+  <button><RouterLink to="/"></RouterLink></button>
+  <h1>Stell dich vor!</h1>
+  <form>
+    <input type="email" name="email" placeholder="E-Mail Adresse" required />
+    <input type="password" name="password" placeholder="Passwort" minlength="8" required />
+    //Länge und Vorgaben des Passworts
+    <input type="password" name="passwordConfirm" placeholder="Passwort bestätigen" required />
+    <input
+      type="text"
+      name="firstname"
+      placeholder="Vorname"
+      maxlength="14"
+      style="text-transform: capitalize"
+      required
+    />
+    <input
+      type="text"
+      name="lastname"
+      placeholder="Nachname"
+      style="text-transform: capitalize"
+      required
+    />
+  </form>
+  <button><router-link to="/register/car">Auto-Gruppe erstellen</router-link></button>
+  <button><router-link to="/join">Auto-Gruppe beitreten</router-link></button>
 </template>
 
 <script>
