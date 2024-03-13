a6a9008ae1b65fa80f491a361ded1a6f22fab83e
Maria Lissel <lisselmaria@gmail.com>
2024-03-13 13:47:51 +0100

    finished viewstart and started reguser

--- src/views/StartView.vue
+++ src/views/StartView.vue
@@ -1,17 +1,19 @@
 <template>
-  <div class="bg-pre-bus-large">
-    <div>
-      <h1>Ready for carma?</h1>
-      <h2>ONE - TWO - YOU</h2>
+  <section>
+    <div class="bg-pre-bus-large">
+      <h1>Ready for <span class="carma-h1">carma</span>?</h1>
+      <h2 class="header-two">ONE - TWO - YOU</h2>
+      <div class="orange-circle">
+        <!--<img src="kreis.png" alt="Kreis orang" class="kreis-orang" />-->
+      </div>
+      <div class="input-container">
+        <button>
+          <router-link to="/register" class="btn-pre-small">neu bei carma?</router-link>
+        </button>
+        <button><router-link to="/login" class="btn-pre-small">log in</router-link></button>
+      </div>
     </div>
-    <div>
-      <!--<img src="kreis.png" alt="Kreis orang" class="kreis-orang" />-->
-    </div>
-    <button>
-      <router-link to="/register" class="btn-pre-small">neu bei carma?</router-link>
-    </button>
-    <button><router-link to="/login" class="btn-pre-small">log in</router-link></button>
-  </div>
+  </section>
 </template>
 
 <script>
@@ -19,7 +21,31 @@ export default {}
 </script>
 
 <style scoped>
-.kreis-orang {
-  position: absolute; /* Absolute Positionierung für das Bild */
+.header-two {
+  position: relative;
+}
+
+.orange-circle {
+  background-image: url('./public/kreis_sonne.png');
+  width: 60px;
+  height: 60px;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  position: absolute;
+  top: 165px;
+  left: 805px;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+
+.carma-h1 {
+  color: var(--blue);
 }
 </style>
