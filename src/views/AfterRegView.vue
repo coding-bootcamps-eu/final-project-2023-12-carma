e6d946694a9cb85af672c53c1a8afc07390c5697
Maria Lissel <lisselmaria@gmail.com>
2024-03-14 13:42:22 +0100

    started main content

--- src/views/AfterRegView.vue
+++ src/views/AfterRegView.vue
@@ -5,15 +5,18 @@
       <!--<button>
         <RouterLink to="/home"><i class="fa-solid fa-circle-chevron-left"></i></RouterLink>
       </button>-->
-      <!---carma-h1 sollte "carma" blau färben-->
-      <h1 class="home-h1">Willkommen bei <span class="carma-h1">carma</span>!</h1>
+      <h1>Willkommen bei <br /><span class="carma-h1">carma</span>!</h1>
       <form>
-        <button>
-          <router-link to="/register/car" class="btn-pre-small">Auto-Gruppe erstellen</router-link>
-        </button>
-        <button>
-          <router-link to="/join" class="btn-pre-small">Auto-Gruppe beitreten</router-link>
-        </button>
+        <div class="btn-container">
+          <button>
+            <router-link to="/register/car" class="btn-pre-small"
+              >Auto-Gruppe erstellen</router-link
+            >
+          </button>
+          <button>
+            <router-link to="/join" class="btn-pre-small">Auto-Gruppe beitreten</router-link>
+          </button>
+        </div>
       </form>
     </div>
   </section>
@@ -26,5 +29,16 @@ export default {}
 <style scoped>
 .carma-h1 {
   color: var(--blue);
+}
+
+.btn-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+
+  position: absolute;
+  bottom: 3.5rem;
 }
 </style>
