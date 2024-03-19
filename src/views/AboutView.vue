9713229cbc4ed26a0142e2647003fc7c9709730f
Christoph Menningen <christoph.menningen@gmail.com>
2024-03-19 09:07:27 +0100

    About angelegt

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,7 +1,45 @@
 <template>
-  <div></div>
+  <div class="deckel"></div>
+  <h1 class="h1-event">carma?</h1>
+  <div class="carma-text">
+    carma ist das Abschlussprojekt eines Webdevelopment-Bootcamps von Doro, Maria, Lorenz und
+    Christoph. Aber <span class="carma">carma</span> ist eigentlich viel mehr.<br /><br /><span
+      class="carma"
+      >carma</span
+    >
+    ist unsere Vision einer nachhaltigen Carsharing-App. Carsharing-App, was soll daran neu sein?
+    Nun, wir nehmen Carsharing wörtlich. Wir möchten, dass Menschen sich Autos teilen, gemeinsam
+    Eigentümer:innen des Wagens sind und so eine nachhaltigere Nutzung schaffen. <br /><br />In
+    Deutschland sind bald 50 Millionen Autos zugelassen, 0,6 pro Einwohner:in. Viele dieser Autos
+    stehen (glücklicherweise) meistens. Wird nur ein kleiner Teil davon von mehreren
+    Eigentümer:innen gemeinsam gefahren, wird die Welt ein kleines Stück besser. <br /><br /><span
+      class="carma"
+      >carma</span
+    >
+    verwaltet diese Autos, die gemeinsame Eigentümer:innen haben und die etwas für ihr Karma-Konto
+    tun wollen.
+  </div>
+
+  <span></span>
 </template>
 
 <script></script>
 
-<style scoped></style>
+<style scoped>
+.carma-text {
+  background-color: var(--beige-light);
+  border: solid 0.1rem var(--orange);
+  border-radius: 0.4rem;
+  padding: 1rem;
+  margin: 2rem;
+  font-size: 17px;
+  line-height: 125%;
+  hyphens: auto;
+  text-align: justify;
+  text-align-last: none;
+}
+
+.carma {
+  color: var(--blue);
+}
+</style>
